036453d5b3e09990a946ae522b9d27628bb0849f
Team Kugel <Maxy@10.1.10.80>
2014-10-16 17:05:29 +0200

    added ball collision detection

--- src/com/example/balanceit/Ball.java
+++ src/com/example/balanceit/Ball.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 
 /**
  * Die Ball-Klasse repräsentiert die Spielkugel.
@@ -21,7 +22,9 @@ class Ball {
     private int mDifficulty;
     private Bitmap mBitmap;
     private Paint mPaint; 
-
+    private int mStartPosX;
+    private int mStartPosY;
+    private int mSpeed;
     //Membervariablen mit bewegten Kugeldaten (aktuelle Position)
     public float mPosX;
     public float mPosY;
@@ -43,11 +46,27 @@ class Ball {
     	mDifficulty=difficulty;
     	mPaint=new Paint();
     	mPaint.setColor(Color.RED);
+		mStartPosX=x;
+		mStartPosY=y; 
+		
+		if (mDifficulty ==0){
+			mSpeed = 4;
+		
+		}
+		if (mDifficulty == 1){
+			mSpeed = 8;
+		}
+		if  (mDifficulty ==2){
+			mSpeed =12;
+		}
     //TODO AP Kugel: restliche Membervariablen initialisieren, Startposition merken
     }
 
     /** setze Kugel auf Startposition zurück */
     public void reset(){
+    	mPosY=mStartPosY;
+       mPosX=mStartPosX;
+    	
     	//TODO AP Kugel: Position zurücksetzen
      }
 
@@ -57,18 +76,32 @@ class Ball {
      */
     public void updatePosition(float sensorX, float sensorY) {
         //TODO AP2: als Testimplementierung die Sensorwerte auf die Kugelposition aufaddieren
-    	mPosX=mPosX+8*sensorX;
-    	mPosY=mPosY+8*sensorY;
+    	mPosX=mPosX+mSpeed*sensorX;
+    	mPosY=mPosY+mSpeed*sensorY;
     	
     	//TODO AP Kugel: Kugelposition mit Sensordaten aktualisieren 
     }
 
     /** korrigiere die Position, wenn die Kugel das Spielfeld verlässt
-     * @param width Spielfeldbreite
+     *  @param width Spielfeldbreite
      * @param height Spielfeldhöhe
      * @return true, wenn eine Kollision mit den Wänden erfolgt ist
      */
     public boolean resolveCollisionWithBounds(int width, int height) {
+    if ( mPosX<0){
+    	mPosX=0;
+    }
+    if (mPosX>width-(2*mRadius)){
+    	mPosX=width-(2*mRadius);
+    }
+    	
+    if(mPosY<0){
+    	mPosY=0;
+    }
+    if(mPosY>height-(2*mRadius)){
+    	mPosY=height-(2*mRadius);
+    }
+    
     	//TODO AP Kugel: Kugelposition innerhalb des Spielfelds halten
     	//TODO AP Kugel: true zurückliefern bei Kollision mit den Wänden 
     	return false;
@@ -81,8 +114,25 @@ class Ball {
      * @return true, wenn Kugelzentrum über aktivem (Rechteck,Kreis)-Bereich der Kachel ist
      */
     public boolean centerIsOnTile(Tile tile) {
+    	//Log.v(World.LOGTAG,"Pos:"+mPosX+","+mPosY);
+    	//Log.v(World.LOGTAG,"Posk:"+tile.mPosX+","+tile.mPosY);
+    	//Log.v(World.LOGTAG,"kt:"+tile.mType);
     	//TODO AP Kugel: Kugelposition testen
-    	return false;
+    	if(tile.mType == Tile.Type.SQUARE){
+    	
+    		if ((mPosX+ mRadius)>tile.mPosX && (mPosX+ mRadius)<tile.mPosX+mTileSize
+    			&&( mPosY+ mRadius)>tile.mPosY && (mPosY +mRadius)<tile.mPosY+mTileSize){
+    		
+    			return true; 
+    		}
+    	}
+    	Log.v(World.LOGTAG,"Pos:"+mPosX+","+mPosY);
+    	if(tile.mType == Tile.Type.CIRCLE){
+			if (Math.sqrt((mPosX-tile.mPosX) * (mPosX-tile.mPosX)+ (mPosY-tile.mPosY)*(mPosY-tile.mPosY)) <= 0.5* mTileSize){
+				return true;
+			}
+		}
+    return false ;
     }
 
     /** berechne X-Abstand von Kugelzentrum zu Kachelzentrum
@@ -90,6 +140,7 @@ class Ball {
      * @return Abstand in X-Koordinatenrichtung
      */
     private float calcDistanceX(Tile tile){
+//    	Math.abs(x);
     	//TODO AP Kugel: Abstand X berechnen
     	return 0.0f;
     }
